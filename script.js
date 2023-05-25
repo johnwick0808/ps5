eb6c9cbd1ad643af6eb78c30aa78ed8a7cbcf1f3
johnwick0808 <134579745+johnwick0808@users.noreply.github.com>
2023-05-25 11:37:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const submit = () => {
+    var Name = document.getElementById("First name").value;
+    var LastName = document.getElementById("Last name").value;
+    var MotherName = document.getElementById("Mother's name").value;
+    var FatherName = document.getElementById("Father's name").value;
+    var Address = document.getElementById("Address").value;
+    var DOB = document.getElementById("DOB").value;
+    var Pincode = document.getElementById("Pincode").value;
+    var Course = document.getElementById("Course").value;
+    var EmailID = document.getElementById("Email ID").value;
+ 
+    console.log(Name);
+    console.log(LastName);
+    console.log(MotherName);
+    console.log(FatherName);
+    console.log(Address);
+    console.log(DOB);
+    console.log(Pincode);
+    console.log(Course);
+    console.log(EmailID);
+}
